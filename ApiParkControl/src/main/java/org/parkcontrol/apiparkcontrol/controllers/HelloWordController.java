ef3bb0f45c6f2db4c13b662740d2d27654a1b842
luisbaquiax <luis.baquiax1995@gmail.com>
2025-10-12 23:43:43 -0600

    add: redis dependence

--- ApiParkControl/src/main/java/org/parkcontrol/apiparkcontrol/controllers/HelloWordController.java
+++ ApiParkControl/src/main/java/org/parkcontrol/apiparkcontrol/controllers/HelloWordController.java
@@ -1,5 +1,7 @@
 package org.parkcontrol.apiparkcontrol.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloWordController {
     @GetMapping
-    public String helloWorld(){
+    public String helloWorld() {
         return "Hello World";
+    }
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @GetMapping("/redis")
+    public String testRedis() {
+        redisTemplate.opsForValue().set("mensaje", "Hola desde Redis en AWS!");
+        return redisTemplate.opsForValue().get("mensaje");
     }
 }
