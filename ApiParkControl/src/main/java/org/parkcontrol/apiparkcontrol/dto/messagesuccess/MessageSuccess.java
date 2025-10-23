ceeff3c0c69e46671cfd2801a835825ff8169e34
luisbaquiax <luis.baquiax1995@gmail.com>
2025-10-22 21:46:51 -0600

    se elimina depedencia duplicada y algunos imports que no estaban bien

--- ApiParkControl/src/main/java/org/parkcontrol/apiparkcontrol/dto/messagesuccess/MessageSuccess.java
+++ ApiParkControl/src/main/java/org/parkcontrol/apiparkcontrol/dto/messagesuccess/MessageSuccess.java
@@ -2,8 +2,10 @@ package org.parkcontrol.apiparkcontrol.dto.messagesuccess;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class MessageSuccess {
     private int code;
