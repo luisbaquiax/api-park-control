686dccc785800783d6ad72508a7a95eb69b35e88
Rudy Reyes <rudyreyes202031213@cunoc.edu.gt>
2025-11-02 01:11:22 -0600

    add: cortes de caja

--- ApiParkControl/src/main/java/org/parkcontrol/apiparkcontrol/repositories/HistorialPagoSuscripcionRepository.java
+++ ApiParkControl/src/main/java/org/parkcontrol/apiparkcontrol/repositories/HistorialPagoSuscripcionRepository.java
@@ -3,5 +3,8 @@ package org.parkcontrol.apiparkcontrol.repositories;
 import org.parkcontrol.apiparkcontrol.models.HistorialPagoSuscripcion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HistorialPagoSuscripcionRepository extends JpaRepository<HistorialPagoSuscripcion, Long> {
+    List<HistorialPagoSuscripcion> findBySuscripcion_Empresa_IdEmpresa(Long idEmpresa);
 }
