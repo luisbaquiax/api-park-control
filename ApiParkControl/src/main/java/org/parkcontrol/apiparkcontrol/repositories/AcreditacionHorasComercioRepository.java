08e2dd4430606e9c34341f0685befba922ca5c70
luisbaquiax <luis.baquiax1995@gmail.com>
2025-11-05 04:14:40 -0600

    arregaldo conflictos

--- ApiParkControl/src/main/java/org/parkcontrol/apiparkcontrol/repositories/AcreditacionHorasComercioRepository.java
+++ ApiParkControl/src/main/java/org/parkcontrol/apiparkcontrol/repositories/AcreditacionHorasComercioRepository.java
@@ -4,10 +4,11 @@ import org.parkcontrol.apiparkcontrol.models.AcreditacionHorasComercio;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public interface AcreditacionHorasComercioRepository extends JpaRepository<AcreditacionHorasComercio, Long> {
    
     AcreditacionHorasComercio findByTicket_Id(Long ticketId);
   
-    List<AcreditacionHorasComercio>  findByConvenio_ComercioAfiliado_Id(Long comercioAfiliadoId);
+    List<AcreditacionHorasComercio> findByConvenio_ComercioAfiliado_Id(Long comercioAfiliadoId);
 }
